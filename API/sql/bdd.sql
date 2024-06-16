ab467963b01bdded712af9476e60ab4182619e1a
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-06-16 23:31:55 +0200

    Updated bdd.sql

--- API/sql/bdd.sql
+++ API/sql/bdd.sql
@@ -8,3 +8,16 @@ CREATE DATABASE Restosducoeur_API;
 
 USE Restosducoeur_API;
 
+--  ------------------------
+--  CREATION DES TABLES
+--  ------------------------
+
+CREATE TABLE IF NOT EXISTS Association (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name_ass VARCHAR(25) NOT NULL,
+    date_create DATE NOT NULL,
+    address VARCHAR(50) NOT NULL,
+    phone CHAR(10) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    activity_area VARCHAR(25) NOT NULL
+);
