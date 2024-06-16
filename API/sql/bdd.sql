acaf2fb171e939028da351571efa5c0ef87e6784
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-06-16 11:25:46 +0200

    Added files dbConfig.js and bdd.sql by folder sql

---
+++ API/sql/bdd.sql
@@ -0,0 +1,10 @@
+--  ------------------------
+--  BDD Restosducoeur_API
+--  ------------------------
+
+DROP DATABASE Restosducoeur_API;
+
+CREATE DATABASE Restosducoeur_API;
+
+USE Restosducoeur_API;
+
